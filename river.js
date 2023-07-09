33be390961ccc7503cce1ebc19047ea425d2c5db
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2023-07-09 14:45:12 -0600

    update

--- river.js
+++ river.js
@@ -1,1 +1,14 @@
 console.log('hello from river again!');
+
+function calculateReadingTime(content) {
+  // The average reading speed in words per minute
+  var wordsPerMinute = 200;
+
+  // Calculate the number of words in the content
+  var wordCount = content.trim().split(/\s+/).length;
+
+  // Calculate the reading time in minutes
+  var readingTime = Math.ceil(wordCount / wordsPerMinute);
+
+  return readingTime;
+}
